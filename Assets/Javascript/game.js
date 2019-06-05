37a68f4d5616c9a4cac09d45d25dfd2540b2fa8f
Brian Corbett <briancorbett@brighthouse.com>
2019-06-05 12:12:47 -0400

    added background image to html page. Fixed gem buttons. added random numbers to js file for the gems/buttons

---
+++ Assets/Javascript/game.js
@@ -0,0 +1,30 @@
+//variables
+var win = 0;
+var lost = 0;
+var compGuess = 0;
+var userGuess = 0;
+
+
+
+$(document).ready(function() {
+
+// Function where computer picks random number.
+
+// Function to start game, in the function reset score to 0 and set target score to 19 - 120
+
+// have crystal set new random value to 1 -20
+
+// connect to HTML
+
+// Function to check if you win or lose and resets the game
+
+// JQ click events
+
+
+
+
+
+
+
+
+})
